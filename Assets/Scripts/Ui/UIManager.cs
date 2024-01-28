adc73929437f1af231aa0537cb4f3945541ed15d
ycx <1796655849@qq.com>
2024-01-28 17:37:28 +0800

    第二关

--- Assets/Scripts/Ui/UIManager.cs
+++ Assets/Scripts/Ui/UIManager.cs
@@ -16,13 +16,31 @@ public class UIManager : MonoBehaviour
         {
             LoseText.SetActive(true);
             StartCoroutine(BackToBeginScene());
+            print("lose");
         }
         else if (TeacherController.isWin)
         {
+            MoneyText.text = "you have" + ScoreManager.MoneyAmount + "money";
             WinText.SetActive(true);
-            StartCoroutine(BackToBeginScene());
+            print("win");
+            if (SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                
+                
+               
+                
+                TeacherController.isWin = false;
+                StartCoroutine(Next());
+               
+
+            }
+            else if(SceneManager.GetActiveScene().buildIndex == 2)
+            {
+                StartCoroutine(BackToBeginScene());
+            }
+ 
         }
-        MoneyText.text = "you have" + ScoreManager.MoneyAmount + "money";
+
     }
 
     IEnumerator BackToBeginScene()
@@ -31,5 +49,13 @@ public class UIManager : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
+    IEnumerator Next()
+    {
 
+        ScoreManager.MoneyAmount = 100;
+        yield return new WaitForSeconds(7f);
+        SceneManager.LoadScene(2);
+        ScoreManager.iswin = false;
+        
+    }
 }
