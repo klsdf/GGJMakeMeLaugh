142a1f20984f454cd6e7ae636d219a4bca987b23
ycx <1796655849@qq.com>
2024-01-28 15:02:08 +0800

    更新

--- Assets/Scripts/ScoreManger/ScoreManager.cs
+++ Assets/Scripts/ScoreManger/ScoreManager.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
-    public static int MoneyAmount = 100;
+    public static int MoneyAmount = 5;
     public static void AddMoney(int money)
     {
         MoneyAmount += money;
